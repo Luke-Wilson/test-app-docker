c0e204b878d0ec3bb46aa6d4df517742bceecda4
Luke Wilson <wilson.palooza@gmail.com>
2017-02-19 18:43:53 -0800

    add Beers factory as well as getBeers and getGlassware requests to server

--- client/services/services.js
+++ client/services/services.js
@@ -1,1 +1,33 @@
 angular.module('yoyo.services', [])
+.factory('Beers', function($http) {
+  return {
+    getBeers: function(id) {
+      return $http({
+        method: 'GET',
+        url: '/api/beers/' + id
+      })
+      .then(response => {
+        return JSON.parse(response.data.body);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+
+    getGlassware: function() {
+      console.log('getting glassware');
+      return $http({
+        method: 'GET',
+        url: '/api/glassware'
+      })
+      .then(response => {
+        console.log('GLASSWARE >>>')
+        console.log(JSON.parse(response.data.body))
+        return JSON.parse(response.data.body);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  };
+});
