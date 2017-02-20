25c3f468027ad2dc0200a77ee5126576370b66dd
Luke Wilson <wilson.palooza@gmail.com>
2017-02-19 18:42:47 -0800

    add controllers and controllerAs to routes in app.js

--- client/app.js
+++ client/app.js
@@ -1,19 +1,32 @@
 angular.module('yoyo', [
   'yoyo.services',
+  'yoyo.beers',
   'ui.router'
 ])
 .config(function($stateProvider) {
   var home = {
     url: '/home',
-    templateUrl: '/home/home.html'
+    templateUrl: '/home/home.html'//,
+    // controller: 'HomeController',
+    // controllerAs: 'HomeVm'
   };
 
   var other = {
     url: '/other',
-    templateUrl: '/other/other.html'
+    templateUrl: '/other/other.html'//,
+    // controller: 'OtherController',
+    // controllerAs: 'OtherVm'
+  };
+
+  var beers = {
+    url: '/beers',
+    templateUrl: '/beers/beers.html',
+    controller: 'BeersController',
+    controllerAs: 'BeersVm'
   };
 
   $stateProvider
     .state('home', home)
-    .state('other', other);
+    .state('other', other)
+    .state('beers', beers);
 });
